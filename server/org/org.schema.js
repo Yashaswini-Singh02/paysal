3776a9a9600926c4adbb6a18478857102576523f
Karan <karanpargal007@gmail.com>
2023-03-16 11:28:02 +0530

    Add basic BE schemas

--- server/org/org.schema.js
+++ server/org/org.schema.js
@@ -5,5 +5,17 @@ const orgSchema = new Schema({
         type: String,
         required: true
     },
+    industry: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    walletAddress: {
+        type: String,
+        required: true
+    },
     
 })
